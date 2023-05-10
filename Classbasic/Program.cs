af783773d4ac27318fbc545ef57ae5b26925d50c
Insori <s2110@e-mirim.hs.kr>
2023-05-10 12:55:42 +0900

    초기 커밋.

---
+++ Classbasic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+            Console.WriteLine("Hello World!");
+        }
+
+        //클래스 안에 클래스를 만들 수 있음
+        class ThirdClass
+        {
+
+        }
+    }
+
+    internal class OtherClass
+    {
+    }
+
+    class SecondClass
+    {
+
+    }
+}
